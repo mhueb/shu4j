6c7d098ce88aff1c8a0c8c583c403815bad58489
mhueb <mhueb@pobox.com>
2014-05-29 23:55:35 +0200

    licence text

--- shu4j-utils/src/main/java/org/shu4j/utils/util/LoggerUtil.java
+++ shu4j-utils/src/main/java/org/shu4j/utils/util/LoggerUtil.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2012 Matthias Huebner
+ * Copyright 2012 SHU4J
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
